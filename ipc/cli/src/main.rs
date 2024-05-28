565747c3a2d2076cd4eb6c2cafe2181eb54845cb
Mikers <snissn@gmail.com>
2024-05-28 00:32:50 -1000

    feat(ipc-cli): enable logging from imported libraries (#890)

--- ipc/cli/src/main.rs
+++ ipc/cli/src/main.rs
@@ -1,9 +1,15 @@
 // Copyright 2022-2024 Protocol Labs
 // SPDX-License-Identifier: MIT
 
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
 #[tokio::main]
 async fn main() {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
 
     if let Err(e) = ipc_cli::cli().await {
         log::error!("main process failed: {e:#}");
