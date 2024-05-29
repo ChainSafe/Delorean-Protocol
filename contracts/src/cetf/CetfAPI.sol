4fcedf2fe8df8227e5095bf181f5858042450535
Willem Olding <willemolding@gmail.com>
2024-05-29 16:13:30 +1000

    rewrite the cbor encoding in the contracts

--- contracts/src/cetf/CetfAPI.sol
+++ contracts/src/cetf/CetfAPI.sol
@@ -10,34 +10,51 @@ import "solidity-cborutils/contracts/CBOR.sol";
 /// @title This library is a proxy to the singleton CETF actor (address: f05)
 /// @author BadBoi Labs
 library CetfAPI {
+    using CBOR for CBOR.CBORBuffer;
+
     CommonTypes.FilActorId constant ActorID = CommonTypes.FilActorId.wrap(49);
-    uint256 constant EnqueueTagMethodNum = 18206847610; // obtained from actors/cetf/actor.rs  actor_dispatch! macro
+    uint256 constant EchoMethodNum = 1638589290; // obtained from actors/cetf/actor.rs  actor_dispatch! macro
+    uint256 constant EnqueueTagMethodNum = 1820684761; // obtained from actors/cetf/actor.rs  actor_dispatch! macro
 
-    struct EnqueueTagParams {
-        bytes tag;
-    }
 
-    /// @notice serialize WithdrawBalanceParams struct to cbor in order to pass as arguments to the market actor
-    /// @param params WithdrawBalanceParams to serialize as cbor
-    /// @return response cbor serialized data as bytes
-    function serializeEnqueueTagParams(EnqueueTagParams memory params) internal pure returns (bytes memory) {
+    function serializeEnqueueTagParams(bytes32 tag) internal pure returns (bytes memory) {
+        // Create a bytes array with length 32
+        bytes memory bytesTag = new bytes(32);
+        
+        // Copy each byte from bytes32 to the bytes array
+        for (uint i = 0; i < 32; i++) {
+            bytesTag[i] = tag[i];
+        }
+
         uint256 capacity = 0;
-        bytes memory tokenAmount = params.tokenAmount.serializeBigInt();
 
-        capacity += Misc.getPrefixSize(2);
-        capacity += Misc.getBytesSize(params.provider_or_client.data);
-        capacity += Misc.getBytesSize(tokenAmount);
+        capacity += Misc.getPrefixSize(1);
+        capacity += Misc.getBytesSize(bytesTag);
+
         CBOR.CBORBuffer memory buf = CBOR.create(capacity);
 
-        buf.startFixedArray(2);
-        buf.writeBytes(params.provider_or_client.data);
-        buf.writeBytes(tokenAmount);
+        buf.startFixedArray(1);
+        buf.writeBytes(bytesTag);
 
         return buf.data();
     }
 
+    function serializeEchoParams() internal pure returns (bytes memory) {
+        CBOR.CBORBuffer memory buf = CBOR.create(1);
+        buf.writeUndefined();
+        return buf.data();
+    }
+
+    function echo() internal returns (int256) {
+        bytes memory rawParams = serializeEchoParams();
+
+        (int256 exitCode,) =
+            Actor.callByID(ActorID, EchoMethodNum, Misc.CBOR_CODEC, rawParams, 0, false);
+        return (exitCode);
+    }
+
     function enqueueTag(bytes32 tag) internal returns (int256) {
-        bytes memory rawParams = abi.encodePacked(tag);
+        bytes memory rawParams = serializeEnqueueTagParams(tag);
 
         (int256 exitCode,) =
             Actor.callByID(ActorID, EnqueueTagMethodNum, Misc.CBOR_CODEC, rawParams, 0, false);
